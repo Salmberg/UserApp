0e2f3f2b469dd5451423cd0cfcc8600cedf50b21
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-04-13 09:57:24 +0200

    Initial Commit

---
+++ UserApp/UserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAppApp.swift
+//  UserApp
+//
+//  Created by David Salmberg on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct UserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
